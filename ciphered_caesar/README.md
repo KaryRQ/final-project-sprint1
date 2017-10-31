87fdde20e09c7cdbc95e83474b20a6c4562c80d2
KaryRQ <darkness.kary@hotmail.com>
2017-10-31 10:42:21 -0600

    cifrado funcionando

--- ciphered_caesar/README.md
+++ ciphered_caesar/README.md
@@ -4,12 +4,11 @@
 1. Enviar un prompt al usuario quien introducirá la frase a cifrar
 2. Si el usuario no introduce ningún valor o introduce un valor númerico se devolvera un alert de error
 3. Si es una frase entonces pasamos a recorrer el arreglo
-4. Se declara la función cipher la cual cifrará la frase del usuario
-5. Se declaran dos arreglos el primero contendrá la frase del usuario y el otro será la que guarde el nuevo arreglo
-conel cifrado realizado
-6. Comenzaremos a recorrer el arreglo, con la longitud del primer arreglo
-7. Se deberan ir agregando los valores en un nuevo arreglo donde se almacenara la posición que les corresponde del cifrado césar
-8. Se aplica la fórmula de cifrado César al arreglo original
+4. Se declaran los arreglos que se usarán en el programa
+5. Se crea la función de cifrado
+6. Comenzaremos a recorrer el arreglo, con la frase introducida por el usuario
+7. Se convierten los elementos del arreglo a ascii y aplicar la fórmula de cifrado César
+8. Despues de aplicar la fórmula y que los valores esten guardados en ascii se deberá retornar a caracter
 9. Los resultados se mostrarán en una nueva variable que será la que permita visualizarlo en pantalla
 10. Fin
 
--- ciphered_caesar/js/app.js
+++ ciphered_caesar/js/app.js
@@ -1,30 +1,36 @@
-
 // Enviar mensaje al usuario
-var message = prompt('Introduce la frase a cifar');
-if(message===null && message ===isNan()){
-  alert ('Introduce una frase válida ');
-}
+  var message = prompt('Introduce la frase a cifar');
+  if(message===null && message ===isNan){
+    alert ('Introduce una frase válida ');
+  }
 
-//crear función de cifrado
-var cipher = function (message){
   //declarar los arreglos que usaremos
-var array = message;
-var newArray=[];
-//creamos el for para recorrer el arreglo en él aplicaremos la fórmula
-  for(i = 0;i <array.length;i ++){
-    /*en newArray iremos aumentando los valores que recorramos con la fórmula de cifrado;
-    al array original aplicaremos la formula de cifrado césar, para esto deberemos convertir los caracteres en ascii
-    y cuando se aplique la fórmula se debe devolver a caracter.
-Entonces, si quisieras desplazar la "A" tres espacios, deberías hacer lo siguiente:
-Convertir “A” en un ordinal (65).
-Sumar 3 a 65, para obtener 68.
-Reconvertir el ordinal 68 a la letra correspondiente (“D”).
-(x-65+n)%26+65*/
-    newArray.push((charCodeAt.array[i]-65+33)%26+65);/*se debe usar String.fromCharCode para devolver a caracter*/
-       //declaramos la variable para imprimir el mensaje en pantalla
-       var outMessage=newArray;
+  var array = message;
+  var cipherArray=[];
+  var decipherArray=[];
+
+  //crear función de cifrado
+  var cipher = function (message){
+  //creamos el for para recorrer el arreglo en él aplicaremos la fórmula
+    for(i = 0;i <array.length;i ++){
+      /*en newArray iremos aumentando los valores que recorramos con la fórmula de cifrado al array original aplicaremos la formula de cifrado césar, para esto deberemos convertir los caracteres en ascii y cuando se aplique la fórmula se debe devolver a caracter.*/
+      cipherArray.push(String.fromCharCode((array[i].charCodeAt()-65+33)%26+65));/*se debe usar String.fromCharCode para devolver a caracter*/
+         //declaramos la variable para imprimir el mensaje en pantalla
+         var outMessage=cipherArray.join();
+       }
+       return alert('Tu frase cifrada es: '+outMessage);
      }
-     return outMessage;
-   }
-  //Llamamos a nuestra función
-   cipher(message);
+    //Llamamos a nuestra función
+     cipher (message);
+
+  var decipher=function(message){
+
+    //(x - n) % 26
+
+    for(j=0;j<array.length;j++){
+      decipherArray.push(String.fromCharCode((array[j].charCodeAt-33)%26));
+      var impMessage=decipherArray;
+    }
+    return alert('Tu frase descifrada es: '+impMessage);
+  }
+  decipher(message);
